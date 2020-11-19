819ef121c3f593bed6213a85da9ad5b4eb3b8054
Sarujan Rajaratnam <sarujanrajaratnam@imac-de-sarujan.home>
2020-11-19 10:27:58 +0100

    Initial version of proBiblio generated by JHipster-6.10.5

---
+++ src/main/java/com/pro/ynov/com/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.pro.ynov.com.repository;
+
+import com.pro.ynov.com.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
